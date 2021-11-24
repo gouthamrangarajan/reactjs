4972107cab5d51b4095ece9c6b464f8d01771edf
Goutham Rangarajan <grangarajan@ana-data.com>
2021-11-24 00:32:42 -0500

    mobile device changes

--- my-portfolio/components/ScrollRowLayout.tsx
+++ my-portfolio/components/ScrollRowLayout.tsx
@@ -1,5 +1,5 @@
 import { NextComponentType, NextPageContext } from "next";
-import { LegacyRef, useRef, useState } from "react";
+import { LegacyRef, useEffect, useRef, useState } from "react";
 import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/solid";
 import useViewportChecker from "../hooks/useViewportChecker";
 import { AnimatePresence, motion } from "framer-motion";
@@ -12,6 +12,11 @@ const ScrollRowLayout: NextComponentType<
   const scrollEl = useRef<HTMLDivElement>();
   const [showLeftScroll, setShowLeftScroll] = useState(false);
   const [showRightScroll, setShowRightScroll] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(991);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
   const [_, setLastEl] = useViewportChecker(
     () => setShowRightScroll(false),
     () => setShowRightScroll(true)
@@ -21,16 +26,14 @@ const ScrollRowLayout: NextComponentType<
     if (scrollEl.current) {
       scrollEl.current.scrollTo({
         behavior: "smooth",
-        left:
-          scrollEl.current.scrollLeft + (window.innerWidth > 991 ? 1000 : 500),
+        left: scrollEl.current.scrollLeft + (windowWidth > 991 ? 1000 : 500),
       });
       setShowLeftScroll(true);
     }
   };
   const scrollLeft = () => {
     if (scrollEl.current) {
-      let left =
-        scrollEl.current.scrollLeft - (window.innerWidth > 991 ? 1000 : 500);
+      let left = scrollEl.current.scrollLeft - (windowWidth > 991 ? 1000 : 500);
       if (left < 0) left = 0;
 
       scrollEl.current.scrollTo({
@@ -49,49 +52,52 @@ const ScrollRowLayout: NextComponentType<
         {subtitle}
       </span>
       <div
-        className={`flex -space-x-40 md:-space-x-64 p-1 overflow-x-hidden w-full
+        className={`flex -space-x-40 md:-space-x-64 p-1 overflow-x-auto lg:overflow-x-hidden w-full
+                    scrollbar-none
                      ${
-                       centered && window.innerWidth > 1023
-                         ? "justify-center"
-                         : ""
+                       centered && windowWidth > 1023 ? "justify-center" : ""
                      }  `}
         ref={scrollEl as LegacyRef<HTMLDivElement>}
       >
         {children}
         <span className="w-0" ref={setLastEl}></span>
       </div>
-      <AnimatePresence>
-        {!centered && showLeftScroll && (
-          <motion.button
-            initial={{ transform: "translateX(-4rem)" }}
-            animate={{ transform: "translateX(0)" }}
-            exit={{ transform: "translateX(-4rem)" }}
-            transition={{ duration: 0.3 }}
-            className="appearance-none outline-none absolute top-0 left-0 rounded-full mt-56
+      {!centered && windowWidth > 991 && (
+        <>
+          <AnimatePresence>
+            {showLeftScroll && (
+              <motion.button
+                initial={{ transform: "translateX(-4rem)" }}
+                animate={{ transform: "translateX(0)" }}
+                exit={{ transform: "translateX(-4rem)" }}
+                transition={{ duration: 0.3 }}
+                className="appearance-none outline-none absolute top-0 left-0 rounded-full mt-56
                ml-1 transition duration-300 bg-pink-600 shadow-2xl opacity-50 hover:opacity-100 p-1 cursor-pointer
               ring-2 ring-pink-600 focus:ring-offset-2 focus:ring-offset-pink-100 ease-in-out"
-            onClick={scrollLeft}
-          >
-            <ChevronLeftIcon className="w-8 h-8 text-white"></ChevronLeftIcon>
-          </motion.button>
-        )}
-      </AnimatePresence>
-      <AnimatePresence>
-        {!centered && showRightScroll && (
-          <motion.button
-            initial={{ transform: "translateX(4rem)" }}
-            animate={{ transform: "translateX(0)" }}
-            exit={{ transform: "translateX(4rem)" }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="appearance-none outline-none absolute top-0 right-0 rounded-full mt-56
+                onClick={scrollLeft}
+              >
+                <ChevronLeftIcon className="w-8 h-8 text-white"></ChevronLeftIcon>
+              </motion.button>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {showRightScroll && (
+              <motion.button
+                initial={{ transform: "translateX(4rem)" }}
+                animate={{ transform: "translateX(0)" }}
+                exit={{ transform: "translateX(4rem)" }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="appearance-none outline-none absolute top-0 right-0 rounded-full mt-56
                  mr-3 transition duration-300 bg-pink-600 shadow-2xl opacity-50 hover:opacity-100 p-1 cursor-pointer
                 ring-2 ring-pink-600 focus:ring-offset-2 focus:ring-offset-pink-100 ease-in-out"
-            onClick={scrollRight}
-          >
-            <ChevronRightIcon className="w-8 h-8 text-white"></ChevronRightIcon>
-          </motion.button>
-        )}
-      </AnimatePresence>
+                onClick={scrollRight}
+              >
+                <ChevronRightIcon className="w-8 h-8 text-white"></ChevronRightIcon>
+              </motion.button>
+            )}
+          </AnimatePresence>
+        </>
+      )}
     </div>
   );
 };
