b2eb1a5a888082d0e35623f1bcf2488a2217aea6
Goutham Rangarajan <grangarajan@ana-data.com>
2022-01-25 16:41:32 -0500

    added w,a,s,d keys for movement along with arrows

--- snake-game/src/hooks/useArrowKeys.ts
+++ snake-game/src/hooks/useArrowKeys.ts
@@ -10,19 +10,27 @@ export const useArrowKeys = (handler: GameDirectionInpFunctionType) => {
   >([]);
   const arrowHandler = (ev: KeyboardEvent) => {
     switch (ev.key) {
-      case "ArrowUp": {
+      case "ArrowUp":
+      case "w":
+      case "W": {
         setKeyboardEventQueue([...keyboardEventQueue, "UP"]);
         break;
       }
-      case "ArrowDown": {
+      case "ArrowDown":
+      case "s":
+      case "S": {
         setKeyboardEventQueue([...keyboardEventQueue, "DOWN"]);
         break;
       }
-      case "ArrowLeft": {
+      case "ArrowLeft":
+      case "a":
+      case "A": {
         setKeyboardEventQueue([...keyboardEventQueue, "LEFT"]);
         break;
       }
-      case "ArrowRight": {
+      case "ArrowRight":
+      case "D":
+      case "d": {
         setKeyboardEventQueue([...keyboardEventQueue, "RIGHT"]);
         break;
       }
