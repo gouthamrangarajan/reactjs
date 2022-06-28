ccfa00a5672e718378a2781174d8bb921333231a
Goutham Rangarajan <grangarajan@ana-data.com>
2022-06-28 13:10:24 -0400

    minor changes to loader

--- my-portfolio/components/Loader.tsx
+++ my-portfolio/components/Loader.tsx
@@ -2,7 +2,7 @@ import { JellyTriangle } from '@uiball/loaders'
 
 function Loader() {
     return (
-        <div className="fixed top-0 left-0 h-screen w-screen bg-slate-700/50 flex items-center justify-center">
+        <div className="fixed top-0 left-0 h-screen w-screen bg-slate-800 flex items-center justify-center">
             <JellyTriangle
                 size={60}
                 speed={1.75}
