cddd589e763cdc5ce88e7118c50ca78e92a3a791
Goutham Rangarajan <grangarajan@ana-data.com>
2022-07-18 08:45:45 -0400

    mad small text for route buttons

--- full-calendar-events/components/RouteBtns.tsx
+++ full-calendar-events/components/RouteBtns.tsx
@@ -6,17 +6,17 @@ function RouteBtns() {
     return (
         <>
             <button className='appearance-none outline-none bg-white  py-1 px-3 transition duration-300 
-                                    hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 '
+                               text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 '
                 onClick={() => {
                     router.push("/");
                 }}>Month</button>
             <button className='appearance-none outline-none bg-white  py-1 px-3 transition duration-300 
-                                    hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 
+                               text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 
                                     disabled:bg-gray-300 disabled:cursor-not-allowed disabled:text-white'
                 disabled>Week</button>
 
             <button className='appearance-none outline-none bg-white  py-1 px-3 transition duration-300 
-                                    hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 '
+                                text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border border-gray-300 '
                 onClick={() => {
                     router.push("/day");
                 }}>Day</button>
