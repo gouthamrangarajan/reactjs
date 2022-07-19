1e4b2337f9003be8e4aec0d92ead8556a30b08df
Goutham Rangarajan <grangarajan@ana-data.com>
2022-07-18 23:24:47 -0400

    week changes

--- full-calendar-events/components/RouteBtns.tsx
+++ full-calendar-events/components/RouteBtns.tsx
@@ -5,22 +5,22 @@ function RouteBtns() {
     const router = useRouter();
     return (
         <>
-            <button className={`appearance-none outline-none ${router.pathname == "/" ? "bg-gray-100" : "bg-white"} py-1 px-3 
-                               transition duration-300 text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border
-                               border-gray-300 font-semibold`}
+            <button className={`appearance-none outline-none ${router.pathname == "/" ? "bg-blue-200 border-blue-300" :
+                "bg-white border-gray-300"} py-1 px-3 transition duration-300 text-sm hover:opacity-90
+                                 focus:bg-gray-50 text-gray-700 border font-semibold`}
                 onClick={() => {
                     router.push("/");
                 }}>Month</button>
-            <button className={`appearance-none outline-none ${router.pathname == "/week" ? "bg-gray-100" : "bg-white"}  py-1 px-3 
-                                transition duration-300 text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border
-                                border-gray-300 font-semibold`}
+            <button className={`appearance-none outline-none ${router.pathname == "/week" ? "bg-blue-200 border-blue-300" :
+                "bg-white border-gray-300"} py-1 px-3 transition duration-300 text-sm hover:opacity-90
+                                 focus:bg-gray-50 text-gray-700 border font-semibold`}
                 onClick={() => {
                     router.push("/week");
                 }}>Week</button>
 
-            <button className={`appearance-none outline-none ${router.pathname == "/day" ? "bg-gray-100" : "bg-white"}  py-1 px-3 
-                                transition duration-300 text-sm hover:opacity-90 focus:bg-gray-50 text-gray-700 border
-                                border-gray-300 font-semibold`}
+            <button className={`appearance-none outline-none ${router.pathname == "/day" ? "bg-blue-200 border-blue-300" :
+                "bg-white border-gray-300"} py-1 px-3 transition duration-300 text-sm hover:opacity-90
+                                 focus:bg-gray-50 text-gray-700 border font-semibold`}
                 onClick={() => {
                     router.push("/day");
                 }}>Day</button>
