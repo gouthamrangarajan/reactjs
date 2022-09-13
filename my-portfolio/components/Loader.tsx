b70a7678987dd378df2cbe51c44dafc072fa4ad0
Goutham Rangarajan <grangarajan@ana-data.com>
2022-09-13 11:24:29 -0400

    changed loader

--- my-portfolio/components/Loader.tsx
+++ my-portfolio/components/Loader.tsx
@@ -1,19 +1,19 @@
-import { JellyTriangle } from '@uiball/loaders'
-import { NextPage } from 'next';
+import { DotPulse } from "@uiball/loaders";
+import { NextPage } from "next";
 
 const Loader: NextPage<LoaderPropsType> = ({ transparent = false }) => {
-    return (
-        <div className={`fixed top-0 left-0 h-screen w-screen bg-slate-800 flex items-center justify-center
-                          ${transparent ? "bg-slate-800/50" : "bg-slate-800"}  `}>
-            <JellyTriangle
-                size={60}
-                speed={1.75}
-                color="#fff"
-            />
-        </div>
-    )
-}
+  return (
+    <div
+      className={`fixed top-0 left-0 h-screen w-screen bg-slate-800 flex items-center justify-center
+                          ${
+                            transparent ? "bg-slate-800/50" : "bg-slate-800"
+                          }  `}
+    >
+      <DotPulse size={40} speed={1.3} color="#fff" />
+    </div>
+  );
+};
 type LoaderPropsType = {
-    transparent?: boolean;
-}
-export default Loader
+  transparent?: boolean;
+};
+export default Loader;
