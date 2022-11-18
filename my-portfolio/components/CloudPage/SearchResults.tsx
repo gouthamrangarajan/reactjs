8ef398befbfd85c59a5d9785f4b5ac4420071302
Goutham Rangarajan <grangarajan@ana-data.com>
2022-11-18 03:54:42 -0500

    changes to web learnings proejct

--- my-portfolio/components/CloudPage/SearchResults.tsx
+++ my-portfolio/components/CloudPage/SearchResults.tsx
@@ -1,44 +1,72 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
 import React from "react";
 import { useContext, useEffect } from "react";
 import { CloudPageContext } from "../../contexts/CloudPageContextProvider";
-import { consolidatedDataType } from "../../models/dataType"
+import { consolidatedDataType } from "../../models/dataType";
 import { AppContext } from "../../pages/_app";
-import ProjectCardList from "../ProjectCardList"
+import ProjectCardList from "../ProjectCardList";
 
 const SearchResults: NextPage<searchResultsPropsType> = ({ cloudData }) => {
-    let { applicationTypeFilter, cloudProviderFilter, textFilter } = useContext(CloudPageContext);
-    let { scrollEl } = useContext(AppContext);
+  let { applicationTypeFilter, cloudProviderFilter, textFilter } =
+    useContext(CloudPageContext);
+  let { scrollEl } = useContext(AppContext);
 
-    useEffect(() => {
-        if (scrollEl && scrollEl.current)
-            scrollEl.current.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    }, [cloudData, cloudProviderFilter, applicationTypeFilter, textFilter, scrollEl]);
+  useEffect(() => {
+    if (scrollEl && scrollEl.current)
+      scrollEl.current.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [
+    cloudData,
+    cloudProviderFilter,
+    applicationTypeFilter,
+    textFilter,
+    scrollEl,
+  ]);
 
-    return (
-        <>
-            <div className="p-1 lg:py-2 lg:px-4 w-full mt-1 min-h-screen">
-                <ProjectCardList data={getCardListData(cloudData, cloudProviderFilter, applicationTypeFilter, textFilter)}></ProjectCardList>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="p-1 lg:py-2 lg:px-4 w-full mt-1 min-h-screen">
+        <ProjectCardList
+          data={getCardListData(
+            cloudData,
+            cloudProviderFilter,
+            applicationTypeFilter,
+            textFilter
+          )}
+        ></ProjectCardList>
+      </div>
+    </>
+  );
+};
 type searchResultsPropsType = {
-    cloudData: consolidatedDataType[]
-}
-export default SearchResults
+  cloudData: consolidatedDataType[];
+};
+export default SearchResults;
 
-function getCardListData(cloudData: consolidatedDataType[], cloudProviderFilter: string,
-    applicationTypeFilter: string, textFilter: string) {
-    let dt: consolidatedDataType[] = cloudData;
-    if (cloudProviderFilter != "")
-        dt = (cloudData.filter(el => el.title.toLowerCase() == cloudProviderFilter.toLowerCase()));
-    else if (applicationTypeFilter != "")
-        dt = (cloudData.filter(el => el.description?.toLowerCase().includes(applicationTypeFilter.toLowerCase())));
-    else if (textFilter != "")
-        dt = (cloudData.filter(el => el.description?.toLowerCase().includes(textFilter.toLowerCase())
-            || el.title.toLowerCase().includes(textFilter.toLowerCase())
-            || el.url.toLowerCase().includes(textFilter.toLowerCase())
-        ));
-    return dt;
+function getCardListData(
+  cloudData: consolidatedDataType[],
+  cloudProviderFilter: string,
+  applicationTypeFilter: string,
+  textFilter: string
+) {
+  let dt: consolidatedDataType[] = cloudData;
+  if (cloudProviderFilter != "")
+    dt = cloudData.filter(
+      (el) =>
+        el.title.toLowerCase() == cloudProviderFilter.toLowerCase() ||
+        el.url.toLowerCase().includes(cloudProviderFilter.toLowerCase())
+    );
+  else if (applicationTypeFilter != "")
+    dt = cloudData.filter((el) =>
+      el.description
+        ?.toLowerCase()
+        .includes(applicationTypeFilter.toLowerCase())
+    );
+  else if (textFilter != "")
+    dt = cloudData.filter(
+      (el) =>
+        el.description?.toLowerCase().includes(textFilter.toLowerCase()) ||
+        el.title.toLowerCase().includes(textFilter.toLowerCase()) ||
+        el.url.toLowerCase().includes(textFilter.toLowerCase())
+    );
+  return dt;
 }
--- my-portfolio/pages/cloud/index.tsx
+++ my-portfolio/pages/cloud/index.tsx
@@ -1,117 +1,161 @@
 import React, { Suspense } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
-const SearchResults = React.lazy(() => import("../../components/CloudPage/SearchResults"));
+const SearchResults = React.lazy(
+  () => import("../../components/CloudPage/SearchResults")
+);
 const Nav = React.lazy(() => import("../../components/Nav"));
-const CloudPageContextProvider = React.lazy(() => import("../../contexts/CloudPageContextProvider"));
-import dataType, { cloudType, consolidatedDataType } from "../../models/dataType";
-const NavSearch = React.lazy(() => import("../../components/CloudPage/NavSearch"));
+const CloudPageContextProvider = React.lazy(
+  () => import("../../contexts/CloudPageContextProvider")
+);
+import dataType, {
+  cloudType,
+  consolidatedDataType,
+} from "../../models/dataType";
+const NavSearch = React.lazy(
+  () => import("../../components/CloudPage/NavSearch")
+);
 import { createClient } from "redis";
 import Loader from "../../components/Loader";
 
 const Index: NextPage<cloudPropsType> = ({ data }) => {
-    return (
-        <>
-            <Head>
-                <title>Goutham Rangarajan - Cloud Projects</title>
-                <meta
-                    key="description"
-                    content="Goutham Rangarajan Full Stack Cloud Projects"
-                    property="description"
-                ></meta>
-                <link href="/favicon.ico" rel="icon" type="image/x-icon"></link>
-                <link href="/manifest.json" rel="manifest"></link>
-            </Head>
-            <Suspense fallback={<Loader></Loader>}>
-                <CloudPageContextProvider>
-                    <div className="w-full bg-slate-700 flex flex-col">
-                        <Nav menu={<NavSearch></NavSearch>}></Nav>
-                        <SearchResults cloudData={data}></SearchResults>
-                    </div>
-                </CloudPageContextProvider>
-            </Suspense>
-        </>)
-}
-type cloudPropsType = {
-    data: consolidatedDataType[];
+  return (
+    <>
+      <Head>
+        <title>Goutham Rangarajan - Cloud Projects</title>
+        <meta
+          key="description"
+          content="Goutham Rangarajan Full Stack Cloud Projects"
+          property="description"
+        ></meta>
+        <link href="/favicon.ico" rel="icon" type="image/x-icon"></link>
+        <link href="/manifest.json" rel="manifest"></link>
+      </Head>
+      <Suspense fallback={<Loader></Loader>}>
+        <CloudPageContextProvider>
+          <div className="w-full bg-slate-700 flex flex-col">
+            <Nav menu={<NavSearch></NavSearch>}></Nav>
+            <SearchResults cloudData={data}></SearchResults>
+          </div>
+        </CloudPageContextProvider>
+      </Suspense>
+    </>
+  );
 };
-export default Index
+type cloudPropsType = {
+  data: consolidatedDataType[];
+};
+export default Index;
 export async function getStaticProps() {
-    let cloudConsolidatedData: consolidatedDataType[] = [];
-    try {
-        const redis_client = createClient({
-            url: process.env.REDIS_URL,
-            password: process.env.REDIS_PWD
-        });
-        redis_client.on('error', (err) => console.log('Redis Client Error', err));
-        await redis_client.connect();
-        let data = JSON.parse(await redis_client.get("portfolio_data") || "{}") as dataType;
-        await redis_client.disconnect();
-        cloudConsolidatedData = getConsolidatedData(data.info.cloud);
-    }
-    catch (err) {
-        console.log('Redis Client Error', err)
-    }
-    if (cloudConsolidatedData.length == 0) {
-        let data: dataType = await import("../../public/data.json");
-        cloudConsolidatedData = getConsolidatedData(data.info.cloud);
-    }
-    return {
-        props: {
-            data: cloudConsolidatedData,
-        },
-        revalidate: 60
-    };
+  let cloudConsolidatedData: consolidatedDataType[] = [];
+  try {
+    const redis_client = createClient({
+      url: process.env.REDIS_URL,
+      password: process.env.REDIS_PWD,
+    });
+    redis_client.on("error", (err) => console.log("Redis Client Error", err));
+    await redis_client.connect();
+    let data = JSON.parse(
+      (await redis_client.get("portfolio_data")) || "{}"
+    ) as dataType;
+    await redis_client.disconnect();
+    cloudConsolidatedData = getConsolidatedData(data.info.cloud);
+  } catch (err) {
+    console.log("Redis Client Error", err);
+  }
+  if (cloudConsolidatedData.length == 0) {
+    let data: dataType = await import("../../public/data.json");
+    cloudConsolidatedData = getConsolidatedData(data.info.cloud);
+  }
+  return {
+    props: {
+      data: cloudConsolidatedData,
+    },
+    revalidate: 60,
+  };
 }
-function getConsolidatedData({ firebase, azure, netlify, cloudflare }: cloudType): consolidatedDataType[] {
-    let consolidated: Array<consolidatedDataType> = [];
-    firebase.forEach(el => {
-        if (el.imgSrc)
-            consolidated.push({ imgSrc: el.imgSrc, url: el.url || "", description: el.description || "", title: "FIREBASE" });
-        if (el.other)
-            el.other.forEach(inEl => {
-                if (inEl.imgSrc)
-                    consolidated.push({
-                        imgSrc: inEl.imgSrc, url: inEl.url || "", description: inEl.description || "",
-                        title: "FIREBASE"
-                    })
-            })
-    });
-    cloudflare.forEach(el => {
-        if (el.imgSrc)
-            consolidated.push({ imgSrc: el.imgSrc, url: el.url || "", description: el.description || "", title: "CLOUDFLARE" });
-        if (el.other)
-            el.other.forEach(inEl => {
-                if (inEl.imgSrc)
-                    consolidated.push({
-                        imgSrc: inEl.imgSrc, url: inEl.url || "", description: inEl.description || "",
-                        title: "NETLIFY"
-                    })
-            })
-    });
-    netlify.forEach(el => {
-        if (el.imgSrc)
-            consolidated.push({ imgSrc: el.imgSrc, url: el.url || "", description: el.description || "", title: "NETLIFY" });
-        if (el.other)
-            el.other.forEach(inEl => {
-                if (inEl.imgSrc)
-                    consolidated.push({
-                        imgSrc: inEl.imgSrc, url: inEl.url || "", description: inEl.description || "",
-                        title: "NETLIFY"
-                    })
-            })
-    });
-    azure.forEach(el => {
-        if (el.imgSrc)
-            consolidated.push({ imgSrc: el.imgSrc, url: el.url || "", description: el.description || "", title: "AZURE" });
-        if (el.other)
-            el.other.forEach(inEl => {
-                if (inEl.imgSrc)
-                    consolidated.push({
-                        imgSrc: inEl.imgSrc, url: inEl.url || "", description: inEl.description || "",
-                        title: "AZURE"
-                    })
-            })
-    });
-    return consolidated;
+function getConsolidatedData({
+  firebase,
+  azure,
+  netlify,
+  cloudflare,
+}: cloudType): consolidatedDataType[] {
+  let consolidated: Array<consolidatedDataType> = [];
+  firebase.forEach((el) => {
+    if (el.imgSrc)
+      consolidated.push({
+        imgSrc: el.imgSrc,
+        url: el.url || "",
+        description: el.description || "",
+        title: "FIREBASE",
+      });
+    if (el.other)
+      el.other.forEach((inEl) => {
+        if (inEl.imgSrc)
+          consolidated.push({
+            imgSrc: inEl.imgSrc,
+            url: inEl.url || "",
+            description: inEl.description || "",
+            title: "FIREBASE",
+          });
+      });
+  });
+  cloudflare.forEach((el) => {
+    if (el.imgSrc)
+      consolidated.push({
+        imgSrc: el.imgSrc,
+        url: el.url || "",
+        description: el.description || "",
+        title: "CLOUDFLARE",
+      });
+    if (el.other)
+      el.other.forEach((inEl) => {
+        if (inEl.imgSrc)
+          consolidated.push({
+            imgSrc: inEl.imgSrc,
+            url: inEl.url || "",
+            description: inEl.description || "",
+            title: "NETLIFY",
+          });
+      });
+  });
+  netlify.forEach((el) => {
+    if (el.imgSrc)
+      consolidated.push({
+        imgSrc: el.imgSrc,
+        url: el.url || "",
+        description: el.description || "",
+        title: "NETLIFY",
+      });
+    if (el.other)
+      el.other.forEach((inEl) => {
+        if (inEl.imgSrc)
+          consolidated.push({
+            imgSrc: inEl.imgSrc,
+            url: inEl.url || "",
+            description: inEl.description || "",
+            title: "NETLIFY",
+          });
+      });
+  });
+  azure.forEach((el) => {
+    if (el.imgSrc)
+      consolidated.push({
+        imgSrc: el.imgSrc,
+        url: el.url || "",
+        description: el.description || "",
+        title: "AZURE",
+      });
+    if (el.other)
+      el.other.forEach((inEl) => {
+        if (inEl.imgSrc)
+          consolidated.push({
+            imgSrc: inEl.imgSrc,
+            url: inEl.url || "",
+            description: inEl.description || "",
+            title: "AZURE",
+          });
+      });
+  });
+  return consolidated;
 }
