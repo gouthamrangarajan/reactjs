ec4b1a4fd6046cf8bdaac0a88ce939d6dc01ba9f
Goutham Rangarajan <grangarajan@ana-data.com>
2023-05-22 10:21:27 -0400

    issue fix in upload with space in value

--- grocery-list/src/data/loaderAndAction.ts
+++ grocery-list/src/data/loaderAndAction.ts
@@ -58,8 +58,8 @@ export const action: ActionFunction = async ({ params, request }) => {
       items = JSON.parse(
         new URL(request.url).search
           .replace("?data=", "")
-          .replaceAll("%22", '"')
-          .replaceAll("%20", '"')
+          .replaceAll("%22", "")
+          .replaceAll("%20", " ")
       ) as Array<Grocery_Item>;
       break;
     }
