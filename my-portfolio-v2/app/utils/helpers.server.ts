c4c7314faa23992123c6067565e41fb83e55cd7c
Goutham Rangarajan <grangarajan@ana-data.com>
2023-10-17 13:21:47 -0400

    read from local instead of redis

--- my-portfolio-v2/app/utils/helpers.server.ts
+++ my-portfolio-v2/app/utils/helpers.server.ts
@@ -1,6 +1,3 @@
-import { createStorage } from "unstorage";
-import redisDriver from "unstorage/drivers/redis";
-import { z } from "zod";
 import {
   type cloudType,
   dataSchema,
@@ -8,28 +5,10 @@ import {
   type githubArrayType,
   type codePenArrayType,
 } from "./schema";
-
-const envSchema = z.object({
-  REDIS_HOST: z.string(),
-  REDIS_PORT: z.string(),
-  REDIS_USER: z.string(),
-  REDIS_PWD: z.string(),
-});
-
-const env = envSchema.parse(process.env);
-const redisStorage = createStorage({
-  driver: redisDriver({
-    url: `reds://${env.REDIS_USER}:${env.REDIS_PWD}@${env.REDIS_HOST}:${env.REDIS_PORT}`,
-    maxRetriesPerRequest: 1,
-    commandTimeout: 500,
-    connectTimeout: 500,
-  }),
-});
+import data from "./../../public/data.json";
 
 export async function getData() {
-  return await redisStorage.getItem<z.infer<typeof dataSchema>>(
-    "portfolio_data",
-  );
+  return dataSchema.parse(data);
 }
 
 export function getCloudConsolidatedData(allCloudData: cloudType) {
