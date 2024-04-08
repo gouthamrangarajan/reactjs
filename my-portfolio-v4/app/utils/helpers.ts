48b75915f4198f12fcfaf0ad5e0b9f38d1a7f480
goutham rangarajan <gouthamrangarajan@gouthams-MBP.fios-router.home>
2024-04-08 06:25:07 -0400

    open props cntnd

--- my-portfolio-v4/app/utils/helpers.ts
+++ my-portfolio-v4/app/utils/helpers.ts
@@ -1,38 +1,38 @@
 export function getSlideUpAnimateClass(ind: number) {
   switch (ind) {
     case 0:
-      return `animate-[slide-up_0.3s_ease-in-out]`;
+      return `animate-[slide-up_0.3s_var(--ease-in-out-1)]`;
     case 1:
-      return `animate-[slide-up_0.6s_ease-in-out]`;
+      return `animate-[slide-up_0.6s_var(--ease-in-out-1)]`;
     case 2:
-      return `animate-[slide-up_0.7s_ease-in-out]`;
+      return `animate-[slide-up_0.7s_var(--ease-in-out-1)]`;
     case 3:
-      return `animate-[slide-up_0.8s_ease-in-out]`;
+      return `animate-[slide-up_0.8s_var(--ease-in-out-1)]`;
     case 4:
-      return `animate-[slide-up_0.9s_ease-in-out]`;
+      return `animate-[slide-up_0.9s_var(--ease-in-out-1)]`;
     case 5:
-      return `animate-[slide-up_1.0s_ease-in-out]`;
+      return `animate-[slide-up_1.0s_var(--ease-in-out-1)]`;
     case 6:
-      return `animate-[slide-up_1.1s_ease-in-out]`;
+      return `animate-[slide-up_1.1s_var(--ease-in-out-1)]`;
     case 7:
-      return `animate-[slide-up_1.2s_ease-in-out]`;
+      return `animate-[slide-up_1.2s_var(--ease-in-out-1)]`;
     case 8:
-      return `animate-[slide-up_1.3s_ease-in-out]`;
+      return `animate-[slide-up_1.3s_var(--ease-in-out-1)]`;
     case 9:
-      return `animate-[slide-up_1.4s_ease-in-out]`;
+      return `animate-[slide-up_1.4s_var(--ease-in-out-1)]`;
     case 10:
-      return `animate-[slide-up_1.5s_ease-in-out]`;
+      return `animate-[slide-up_1.5s_var(--ease-in-out-1)]`;
     case 11:
-      return `animate-[slide-up_1.6s_ease-in-out]`;
+      return `animate-[slide-up_1.6s_var(--ease-in-out-1)]`;
     case 12:
-      return `animate-[slide-up_1.7s_ease-in-out]`;
+      return `animate-[slide-up_1.7s_var(--ease-in-out-1)]`;
     case 13:
-      return `animate-[slide-up_1.8s_ease-in-out]`;
+      return `animate-[slide-up_1.8s_var(--ease-in-out-1)]`;
     case 14:
-      return `animate-[slide-up_1.9s_ease-in-out]`;
+      return `animate-[slide-up_1.9s_var(--ease-in-out-1)]`;
     case 15:
-      return `animate-[slide-up_2.0s_ease-in-out]`;
+      return `animate-[slide-up_2.0s_var(--ease-in-out-1)]`;
     default:
-      return `animate-[slide-up_2.1s_ease-in-out]`;
+      return `animate-[slide-up_2.1s_var(--ease-in-out-1)]`;
   }
 }
